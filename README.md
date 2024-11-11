55847dea94a8cf48c1dc1948aed08257462f2f2b
clurect <wright.stephen.m@gmail.com>
2024-11-11 14:46:15 -0500

    making the example

---
+++ README.md
@@ -0,0 +1,76 @@
+# Jancy Example Namath Provider
+
+## Structure of the Plug-in
+- `example-namath-purchase-provider/package.json` - A simple file that describes the plugin to Jancy and specifies an entry point. `src/index.js` in this example.
+- `example-namath-purchase-provider/src/index.js` - This is the main file that implements most of our plugins logic. The most important part is the object exported that has a couple of very specific jancy properties and functions (jancy_props, jancy_onInit, jancy_onEnabled, jancy_onDisabled). That's probably where you should start
+
+## Adding the Plug-in to Jancy
+
+1. Open the Jancy plug-in folder
+    - On Windows, open Windows File Explorer and go to %appdata%\Jancy\plugins or on MacOS
+    - On MacOS, open Finder and go to `~/Library/Application Support/Jancy/plugins`
+2. Copy `example-namath-purchase-provider` folder to the plug-ins folder from step 1.
+3. Restart Jancy if it's already running.
+4. If everything works correctly you should see the example plugin in the list of plugins in `File -> Settings -> Plugins`.
+5. Go to the Namath settings panel (`File -> Setting -> Namath`)
+6. Click the + button and select the "Create a Namath Instance that sends to ExampleNamathPurchaseProvider" option
+7. In the dialog that appears type in a Key and a Name. You will have to edit the dialog code to have the fields you want for your setup.
+8. Press the "Add" button.
+9. If all works correctly, you should see an entry for the provider instance you just created. You can press the test button to see if it works
+10. You can go to TicketMaster and use Namath as you would normally and send to your provider
+
+# Data Structures
+
+## Cart
+
+The cart is an object the you will be using to get the data for your service
+### Properties
+
+#### uuid
+The unique identifier for the purchase.
+#### cartUpdated
+The timestamp of the last time the cart was updated.
+#### fieldColors
+An object that maps field names to colors. Mostly used internally, but you can use it.
+#### tab
+The tab ID that the purchase is associated with.
+#### row
+The row number of the seats in the purchase.
+#### section
+The section number of the seats in the purchase.
+#### seats
+A comma-separated list of the seat numbers in the purchase.
+#### event
+The name of the event in the purchase.
+#### venue
+The name of the venue where the event is taking place.
+#### quantity
+The number of seats for all tickets in the purchase.
+#### currency
+The currency of the purchase (USD, CAN, etc).
+#### total
+The total price of the purchase, includes fees and taxes.
+#### fees
+The total amount of fees associated with the tickets in the purchase.
+#### tax
+The total amount of tax associated with the tickets in the purchase.
+#### costPerTicket
+The cost of each seat in the purchase, excluding fees and tax.
+#### dateTime
+The date and time of the event in the purchase.
+#### oddEven
+Whether or not the row number of the seats in the purchase is odd or even.
+#### deliveryMethod
+The delivery method for the tickets in the purchase.
+#### isInsured
+Whether or not the tickets in the purchase are insured.
+#### marketplace
+The marketplace where the tickets in the purchase were purchased.
+#### feeAndCostPerTicket
+The total cost of each seat in the purchase, including fees and taxes.
+#### ticketType
+The type of ticket in the purchase.
+#### feesPerTicket
+The amount of fees associated with each seat in the purchase.
+#### orderNumber
+The order number that is for the purchase
---
+++ example-namath-purchase-provider/src/index.js
@@ -0,0 +1,253 @@
+const {EventEmitter} = require('events')
+let myMessageAPI = null
+
+const ExampleNamathPurchaseProviderFactory = {
+  id: 'cf88699f-c9a7-426e-9fbb-74699b5d94c1', //generate a uuid that you want
+  description: 'Create a Namath Instance that sends purchases to ExampleNamathPurchaseProvider', //this text shows up in Namath when users click the '+' button to add a provider
+  showFactoriesToUser: true,
+  persistProviders: true,
+  createProvider(jancy, state=null) {
+    return new ExampleNamathPurchaseProvider(jancy, state)
+  },
+  addProvider(jancy, browserWindow) {
+    providerDialog(jancy, browserWindow)
+  }  
+}
+
+/**
+ ** Cart object is an object that is sent to the provider to be handled
+ * {
+ *   "uuid": "b2650836-b281-4ea5-9474-89312cbe8f51",
+ *   "cartUpdated": new Date(), // this is a date object that indicates when the cart was last updated
+ *   "fieldColors": {}, // this is an object that contains the colors of the fields in the purchase, mostly internal
+ *   "tab": "d7700243", // this is the tab id that the purchase was sent from
+ *   "row": null, //  the rows of the tickets in the purchase
+ *   "section": null, // the section of the tickets in the purchase
+ *   "seats": null, // the seats of the tickets in the purchase
+ *   "event": null, // the event name
+ *   "venue": null, //  the venue name
+ *   "quantity": null, // the quantity of tickets in the purchase
+ *   "currency": null, // the currency of the purchase (USD, CAD, etc)
+ *   "total": null, // the total of the purchase
+ *   "fees": null, // the fees of the purchase
+ *   "tax": null, // the tax of the purchase
+ *   "costPerTicket": null, // the cost per seat of the purchase (to be renamed to costPerSeat)
+ *   "dateTime": null, // the date and time of the event
+ *   "oddEven": false, // this is a flag that indicates that the tickets are odd/even
+ *   "deliveryMethod": null, // the delivery method of the purchase
+ *   "isInsured": null, // this is a flag that indicates that the purchase is insured
+ *   "marketplace": null, // the marketplace that the purchase was sent from
+ *   "feeAndCostPerTicket": null, // the fees and cost per seat of the purchase (to be renamed to feeAndCostPerSeat)
+ *   "ticketType": null, // the ticket type of the purchase
+ *   "feesPerTicket": null // the fees per seat of the purchase (to be renamed to feesPerSeat)
+ *   "orderNumber": null, // the order number of the purchase
+ * }
+ */
+
+/**
+ * This is an example of a service that you would create to send messages to your service
+ * the constructor is called with the provider's state object and the jancy object
+ */
+class ExampleNamathPurchaseProvider {
+  constructor(jancy, { key, providerName, instance_id } = {}) {
+    this.jancy = jancy
+    console.log(providerName, key, instance_id)
+    this.name = providerName || `This example name`
+    this.key = key //could be a key or token, but you are in control of these variables
+    this.instance_id = instance_id
+    this.type = 1 // this is the type of provider, 1 is a purchase provider
+  }
+  getName() {
+    return this.name
+  }
+  getInfo() {
+    return `Some friendly info about this provider will be displayed in namath`
+  }
+  /**
+   * Send the cart to where ever you want to send it
+   * @param {*} param0 - {cart is the cart object with all the sale info
+   * tab is the jancy tab object that the cart was sent from,
+   * respFunction is a callback that you call when you get a response from the provider
+   * }
+   */
+  sendPurchaseOrder({cart, tab}) {
+    //you need to send these args to myMessageAPI.sendPurchase
+    let args = {
+      cart: cart
+    }
+    myMessageAPI.sendPurchase({key:this.key}, args)
+    return true
+  }
+  /** Return an object that can be fed back into the constructor to
+  * recreate this provider. This is needed to restore the provider 
+  * after Jancy restarts
+  */
+  getState() {
+    return  {
+      providerName: this.name,
+      key: this.key,
+      instance_id: this.instance_id,
+      type: this.type
+    }
+  }
+  test() {
+    myMessageAPI.sendTest()
+  }
+  editProvider(jancy, browserWindow, provider) {
+    if (!provider) {
+      console.error('No provider to edit')
+      return
+    }
+    providerDialog(jancy, browserWindow, provider, true)
+  }
+}
+
+/**
+ * This dialog will be shown to the user when they click the '+' button in namath.
+ * In it you should display values that allows the user to choose the specifics of your service provider
+ * needed to send namath carts.
+ * It's invocation is handled in the provider factory
+ * @param {*} jancy (Jancy)
+ * @param {*} browserWindow (BrowserWindow) 
+ * @param {*} provider (NamathProvider): optional - for editing an existing provider (namath isn't ready for this yet)
+ */
+
+function providerDialog (jancy, browserWindow, provider=null, edit=false) {
+
+  let providerWindow = this.jancy.dialogFactory.create(
+    browserWindow,
+    {
+      width: 400,
+      height: 150,
+      title: provider ? `Edit ${ provider.getName() }` : 'Configure Example Namath Purchase Provider'
+    },
+    {
+      centerRelativeToParent: true
+    }
+  )
+
+  jancy.actionRegistry.register("example-namath-provider:save", (args, sender) => {
+    /* args is the provider state object essentially
+    args = {
+      key: '1234',
+      providerName: 'My Provider Name',
+      instance_id: '1234'
+    }
+    */
+   if (!provider) {
+    args.instance_id = '1234' //this is a uuid that you generate and in this example can be used to identify the provider
+   }
+    const namath = jancy.getInterface('namathAPI')
+    if (namath) {
+      console.log(args)
+      if (edit) {
+        Object.assign(provider, args)
+        namath.editProvider(provider)
+      }
+      else {
+        namath.createProvider(ExampleNamathPurchaseProviderFactory, args)
+      }
+    }
+  })
+
+  providerWindow.on('ready-to-show', () => {
+
+    const css = `
+      p {
+        padding: 5px 0;
+      }
+      .input-textfield {
+        flex-grow: 1;
+      }
+      .button-container {
+        width: 100%;
+        justify-content: flex-end;
+      }
+    `
+
+    providerWindow.webContents.insertCSS(css)
+
+    const html = `
+      <div class="block my-content">
+        <div class="inline-block">
+          <label class="input-label">Key</label>
+          <input class="input-textfield" name="key" value="${ provider ? provider.key : ""}" />
+        </div>
+        <div class="inline-block">
+          <label class="input-label">Name</label>
+          <input class="input-textfield" name="providerName" value="${ provider ? provider.name : ""}" />
+        </div>
+        <div class="inline-block button-container">
+          <button class="button" onclick="window.close()">Cancel</button>
+          <button class="button" onclick="window.onSave()">${ provider ? "Update" : "Add" }</button>
+        </div>
+      </div>
+    `
+    // this is the code that will be executed in the browser window, it will set up the html and JS needed for the dialog
+    const code = `
+      (function () {
+        window.onSave = (event) => {
+          const key = document.body.querySelector('input[name="key"]').value
+          const providerName = document.body.querySelector('input[name="providerName"]').value
+          if (key.trim().length > 0 && providerName.trim().length > 0) {
+            window.jancyAPI.dispatchAction("example-namath-provider:save", {
+              key,
+              providerName
+            })
+            window.close()
+          }
+        }
+        document.body.innerHTML = \`${html}\`
+      })()
+    `
+
+    providerWindow.on('close', () => {
+      jancy.actionRegistry.unregister("example-namath-provider:save")
+    })
+
+    providerWindow.webContents.executeJavaScript(code)
+    providerWindow.show()
+  })
+}
+class MyMessageAPI extends EventEmitter {
+  
+  constructor(jancy) {
+    super()
+    this.jancy = jancy
+    this.purchases = []
+  }
+  sendPurchase({key}, {cart, wantsResponse}) {
+    console.log('purchase sent to with key', key)
+    this.purchases.push(cart)
+  }
+  sendTest() {
+    console.log('test sent')
+  }
+}
+
+module.exports = {
+
+  jancy_props: {
+    registryVersion: 1
+  },
+
+  /* --------------------------------------------------------------------------
+  ** Called by the pluginRegistry when we are loaded.
+  ** ------------------------------------------------------------------------*/
+  jancy_onInit(jancy, enabled) {
+    
+    myMessageAPI = new MyMessageAPI(jancy)
+
+    //check to see if namath is ready to add this provider factory
+    const namath_check = setInterval(() => {
+      const namath = jancy.getInterface('namathAPI')
+      if (namath) {
+        namath.addFactory(ExampleNamathPurchaseProviderFactory)
+        clearInterval(namath_check)
+      }
+      
+    }, 500);
+
+    jancy.registerInterface('myMessageAPI', myMessageAPI)
+  },
+}
